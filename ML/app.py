1a5e90aa04918df0206a4eeea114c4225c7aa945
Evenn <pateleven8@gmail.com>
2026-01-19 18:44:57 +0530

    setup

--- ML/app.py
+++ ML/app.py
@@ -172,4 +172,6 @@ def analyze():
 # ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
     logging.info("Starting ML service on port 5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(host="0.0.0.0", port=5000, debug=True)
